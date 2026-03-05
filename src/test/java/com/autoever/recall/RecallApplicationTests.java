51379f8de5f05036f45f5a093b5fab871036f56c
seungwoo <rio970319@gmail.com>
2026-03-05 17:11:42 +0900

    프로젝트 생성

---
+++ src/test/java/com/autoever/recall/RecallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoever.recall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

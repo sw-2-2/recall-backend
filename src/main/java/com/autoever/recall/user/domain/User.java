f7139330e9abf6ac653145cd58f3baf0da338b51
seungwoo <rio970319@gmail.com>
2026-03-06 15:51:27 +0900

    #1 feat: User 선언

---
+++ src/main/java/com/autoever/recall/user/domain/User.java
@@ -0,0 +1,34 @@
+package com.autoever.recall.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/autoever/recall/user/domain/UserRole.java
@@ -0,0 +1,5 @@
+package com.autoever.recall.user.domain;
+
+public enum UserRole {
+    ADMIN, USER
+}

ecc363525ac80be5a0e5d27a51a841860bb1074a
Ga Min Cha <prettygamin@naver.com>
2026-03-09 16:36:22 +0900

    #10 feat: SchoolRepository(학교 리스트 조회) 구현

---
+++ src/main/java/com/autoever/recall/school/repository/SchoolRepository.java
@@ -0,0 +1,15 @@
+package com.autoever.recall.school.repository;
+
+import com.autoever.recall.school.domain.School;
+import com.autoever.recall.school.domain.SchoolType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SchoolRepository extends JpaRepository<School, Long> {
+    // 학교 리스트 조회 (키워드를 포함, type='all'일 때)
+    List<School> findByNameContaining(String keyword);
+
+    // 학교 리스트 조회 (키워드를 포함, 특정 타입이 주어질 떄)
+    List<School> findByTypeAndNameContaining(SchoolType type, String keyword);
+}

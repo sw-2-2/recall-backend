a751f03d2c968c4836d3b8915c2a6087e5df5424
Ga Min Cha <prettygamin@naver.com>
2026-03-09 10:51:48 +0900

    #6 feat: ProfileSchool, Profile 모델 연결

--- src/main/java/com/autoever/recall/profileschool/domain/ProfileSchool.java
+++ src/main/java/com/autoever/recall/profileschool/domain/ProfileSchool.java
@@ -1,8 +1,12 @@
 package com.autoever.recall.profileschool.domain;
 
 import com.autoever.recall.school.domain.School;
+import com.autoever.recall.user.domain.Profile;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -19,9 +23,9 @@ public class ProfileSchool {
     private Long id;
 
     // N:1 관계 (FK 관리)
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "profile_id", nullable = false)
-//    private Profile profile;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", nullable = false)
+    private Profile profile;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "school_id", nullable = false)
@@ -34,10 +38,9 @@ public class ProfileSchool {
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
-    // ! 나중에 Profile 연관관계 주석 해제하면, 빌더 생성자 파라미터에도 추가하기
     @Builder
-    public ProfileSchool(School school, Integer graduationYear) {
-        // this.profile = profile;
+    public ProfileSchool(Profile profile, School school, Integer graduationYear) {
+        this.profile = profile;
         this.school = school;
         this.graduationYear = graduationYear;
     }

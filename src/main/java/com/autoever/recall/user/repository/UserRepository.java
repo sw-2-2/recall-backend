f8dc33476bfeba26a885790dd83180bf6991c19e
seungwoo <rio970319@gmail.com>
2026-03-09 16:39:24 +0900

    #9 feat: UserRepository 쿼리 수정

--- src/main/java/com/autoever/recall/user/repository/UserRepository.java
+++ src/main/java/com/autoever/recall/user/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.autoever.recall.user.repository;
 import com.autoever.recall.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -21,9 +20,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     * Profile 포함
     * */
     @Query("""
-        SELECT u FROM User
+        SELECT u FROM User u
         LEFT JOIN FETCH u.profile
         WHERE u.id = :id
     """)
-    Optional<User> findByIdWithProfile(@Param("id") Long id);
+    Optional<User> findByIdWithProfile(Long id);
 }

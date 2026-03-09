1835c274c0f05bad7935fe835d069be03edaabb9
Ga Min Cha <prettygamin@naver.com>
2026-03-09 10:31:07 +0900

    #2 feat: SchoolTypeDto 선언

--- src/main/java/com/autoever/recall/school/dto/SchoolDto.java
+++ src/main/java/com/autoever/recall/school/dto/SchoolDto.java
@@ -1,13 +1,12 @@
 package com.autoever.recall.school.dto;
 
 import com.autoever.recall.school.domain.School;
-import com.autoever.recall.school.domain.SchoolType;
 
 import java.time.LocalDateTime;
 
 public record SchoolDto(
         Long id,
-        SchoolType type,
+        String type,
         String name,
         String imageUrl,
         String address,
@@ -16,7 +15,7 @@ public record SchoolDto(
     public static SchoolDto from(School school) {
         return new SchoolDto(
                 school.getId(),
-                school.getType(),
+                SchoolTypeDto.from(school.getType()),
                 school.getName(),
                 school.getImageUrl(),
                 school.getAddress(),

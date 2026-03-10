72bfface799190c6c9997e5db11e478d19767ec4
seungwoo <rio970319@gmail.com>
2026-03-10 17:19:26 +0900

    #29 feat: UserResponse 프로퍼티 추가

--- src/main/java/com/autoever/recall/user/dto/UserResponse.java
+++ src/main/java/com/autoever/recall/user/dto/UserResponse.java
@@ -1,13 +1,20 @@
 package com.autoever.recall.user.dto;
 
+import com.autoever.recall.school.dto.SchoolDto;
 import com.autoever.recall.user.domain.User;
+import com.autoever.recall.userschool.domain.UserSchool;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public record UserResponse(
         Long id,
         String email,
         String name,
         String phone,
-        String address
+        String address,
+        LocalDateTime updatedAt,
+        List<SchoolDto> schools
 ) {
     public static UserResponse from(User user) {
         return new UserResponse(
@@ -15,7 +22,12 @@ public record UserResponse(
                 user.getEmail(),
                 user.getName(),
                 user.getPhone(),
-                user.getAddress()
+                user.getAddress(),
+                user.getUpdatedAt(),
+                user.getUserSchools().stream()
+                        .map(UserSchool::getSchool)
+                        .map(SchoolDto::from)
+                        .toList()
         );
     }
 }

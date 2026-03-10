b185026223cb1e01c46ece9eae359b3470f4e22c
seungwoo <rio970319@gmail.com>
2026-03-10 18:49:44 +0900

    #29 feat: UserService 구현

--- src/main/java/com/autoever/recall/user/service/UserService.java
+++ src/main/java/com/autoever/recall/user/service/UserService.java
@@ -1,12 +1,24 @@
 package com.autoever.recall.user.service;
 
-import com.autoever.recall.user.domain.UserCreateCommand;
 import com.autoever.recall.user.domain.User;
+import com.autoever.recall.user.domain.UserUpdateCommand;
 
 public interface UserService {
+    /*
+    * UserSchool 미포함
+    * */
     User findByEmail(String email);
 
+    /*
+     * UserSchool 미포함
+     * */
+    User findById(Long id);
+
+    /*
+     * UserSchool 포함
+     * School 포함
+     * */
     User getUser(Long id);
 
-    User registerProfile(UserCreateCommand command);
+    User updateUser(UserUpdateCommand command);
 }
--- src/main/java/com/autoever/recall/user/service/UserServiceImpl.java
+++ src/main/java/com/autoever/recall/user/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.autoever.recall.user.service;
 
-import com.autoever.recall.user.domain.UserCreateCommand;
 import com.autoever.recall.user.domain.User;
+import com.autoever.recall.user.domain.UserUpdateCommand;
 import com.autoever.recall.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,19 +14,28 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
-    public User registerProfile(UserCreateCommand command) {
-        return null;
-    }
-
-    @Override
     public User findByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalStateException("해당하는 유저를 찾을 수 없습니다. email: " + email));
     }
 
     @Override
-    public User getUser(Long id) {
-        return userRepository.findByIdWithProfileAndSchools(id)
+    public User findById(Long id) {
+        return userRepository.findById(id)
                 .orElseThrow(() -> new IllegalStateException("해당하는 유저를 찾을 수 없습니다. id: " + id));
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return userRepository.findByIdWithSchools(id)
+                .orElseThrow(() -> new IllegalStateException("해당하는 유저를 찾을 수 없습니다. id: " + id));
+    }
+
+    @Override
+    @Transactional
+    public User updateUser(UserUpdateCommand command) {
+        User user = findById(1L); // TODO: JWT로 userId 받기
+        user.update(command);
+        return user;
     }
 }

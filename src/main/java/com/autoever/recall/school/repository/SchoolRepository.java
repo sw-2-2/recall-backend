9120333377db818898fd6e2e8798520db16131a1
seungwoo <rio970319@gmail.com>
2026-03-11 12:34:01 +0900

    #29 feat: 누락 어노테이션 추가

--- src/main/java/com/autoever/recall/school/repository/SchoolRepository.java
+++ src/main/java/com/autoever/recall/school/repository/SchoolRepository.java
@@ -3,9 +3,11 @@ package com.autoever.recall.school.repository;
 import com.autoever.recall.school.domain.School;
 import com.autoever.recall.school.domain.SchoolType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface SchoolRepository extends JpaRepository<School, Long> {
     // 학교 리스트 조회 (키워드를 포함, type='all'일 때)
     List<School> findByNameContaining(String keyword);
--- src/main/java/com/autoever/recall/user/repository/UserRepository.java
+++ src/main/java/com/autoever/recall/user/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.autoever.recall.user.repository;
 import com.autoever.recall.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 

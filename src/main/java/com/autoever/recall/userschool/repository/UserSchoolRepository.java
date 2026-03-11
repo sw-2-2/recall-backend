c289f6623554ec10489a47ac4db48e9ae30fe43b
Ga Min Cha <prettygamin@naver.com>
2026-03-11 17:41:35 +0900

    #30 feat: Repository 어노테이션 추가

--- src/main/java/com/autoever/recall/userschool/repository/UserSchoolRepository.java
+++ src/main/java/com/autoever/recall/userschool/repository/UserSchoolRepository.java
@@ -4,9 +4,11 @@ import com.autoever.recall.userschool.domain.UserSchool;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserSchoolRepository extends JpaRepository<UserSchool, Long> {
     // 학교 멤버 리스트 조회
     @Query("SELECT DISTINCT us FROM UserSchool us " +

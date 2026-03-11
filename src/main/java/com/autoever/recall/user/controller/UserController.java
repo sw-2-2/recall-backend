f3b0e2af514a1fe87e4b4cc0d29fc395a86fcae8
seungwoo <rio970319@gmail.com>
2026-03-11 11:06:41 +0900

    #29 feat: User 생성 api 구현

--- src/main/java/com/autoever/recall/user/controller/UserController.java
+++ src/main/java/com/autoever/recall/user/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.autoever.recall.user.controller;
 
+import com.autoever.recall.user.domain.User;
+import com.autoever.recall.user.domain.UserCreateCommand;
 import com.autoever.recall.user.domain.UserUpdateCommand;
-import com.autoever.recall.user.dto.UserResponse;
-import com.autoever.recall.user.dto.UserUpdateRequest;
-import com.autoever.recall.user.dto.UserUpdateResponse;
+import com.autoever.recall.user.dto.*;
 import com.autoever.recall.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserCreateResponse> createUser(@RequestBody @Valid UserCreateRequest request) {
+        UserCreateCommand command = request.toDomain();
+        User user = userService.createUser(request.email(), command);
+        UserCreateResponse response = UserCreateResponse.from(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
     @GetMapping("/me")
     public ResponseEntity<UserResponse> getUser() {

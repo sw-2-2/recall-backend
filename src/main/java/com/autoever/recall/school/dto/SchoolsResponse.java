d61f6c3b03c4f9895036e312a0bdaa6e6c4638ee
Ga Min Cha <prettygamin@naver.com>
2026-03-12 17:47:32 +0900

    #35 refactoring: dtoList 복수형으로

--- src/main/java/com/autoever/recall/school/dto/SchoolsResponse.java
+++ src/main/java/com/autoever/recall/school/dto/SchoolsResponse.java
@@ -5,8 +5,8 @@ import com.autoever.recall.school.domain.School;
 import java.util.List;
 
 public record SchoolsResponse(List<SchoolDto> schools) {
-    public static SchoolsResponse from(List<School> schoolList) {
-        List<SchoolDto> dtoList = schoolList.stream()
+    public static SchoolsResponse from(List<School> schools) {
+        List<SchoolDto> dtoList = schools.stream()
                                             .map(SchoolDto::from)
                                             .toList();
 

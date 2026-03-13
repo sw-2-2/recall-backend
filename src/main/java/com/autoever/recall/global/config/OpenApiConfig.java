8c4eef4c565ee9e7e3ad62954069b1556773c1b2
seungwoo <rio970319@gmail.com>
2026-03-13 11:08:53 +0900

    #42 feat: 기존 cors 파일 제거 및 swagger 설정 수정

--- src/main/java/com/autoever/recall/global/config/OpenApiConfig.java
+++ src/main/java/com/autoever/recall/global/config/OpenApiConfig.java
@@ -1,6 +1,9 @@
 package com.autoever.recall.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +12,18 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
+        String sessionCookieName = "JSESSIONID";
+
+        SecurityScheme sessionAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.COOKIE)
+                .name(sessionCookieName);
+
+        SecurityRequirement requirement = new SecurityRequirement().addList(sessionCookieName);
+
         return new OpenAPI()
-                .addServersItem(new Server().url("/").description("Default Server"));
+                .addServersItem(new Server().url("/").description("Default Server"))
+                .components(new Components().addSecuritySchemes(sessionCookieName, sessionAuth))
+                .addSecurityItem(requirement);
     }
 }

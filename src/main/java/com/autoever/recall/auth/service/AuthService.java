e44d6802699fad7b20926cfe3b68d36fffd190cf
seungwoo <rio970319@gmail.com>
2026-03-13 15:21:51 +0900

    #42 feat: AuthService 구현

--- src/main/java/com/autoever/recall/auth/service/AuthService.java
+++ src/main/java/com/autoever/recall/auth/service/AuthService.java
@@ -1,5 +1,9 @@
 package com.autoever.recall.auth.service;
 
+import org.springframework.security.core.Authentication;
+
 public interface AuthService {
     Long getSessionUserId();
+
+    Authentication login(String email, String password);
 }
--- src/main/java/com/autoever/recall/auth/service/AuthServiceImpl.java
+++ src/main/java/com/autoever/recall/auth/service/AuthServiceImpl.java
@@ -2,16 +2,29 @@ package com.autoever.recall.auth.service;
 
 import com.autoever.recall.auth.service.domain.SessionUserDetails;
 import com.autoever.recall.auth.service.exception.UnAuthorizedException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+    private final AuthenticationManager authenticationManager;
+
     @Override
     public Long getSessionUserId() {
         return getSessionUserDetails().getId();
+    }
+
+    @Override
+    public Authentication login(String email, String password) {
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(email, password);
+        return authenticationManager.authenticate(token);
     }
 
     private SessionUserDetails getSessionUserDetails() {
